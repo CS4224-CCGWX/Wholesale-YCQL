70f5524fa4cf7f22415c072b88d0c4d9dab2f9f0
Chi Xu <chixucontinue@gmail.com>
2022-10-02 21:28:05 +0800

    Refactor to use prepared statements

--- src/main/java/transaction/AbstractTransaction.java
+++ src/main/java/transaction/AbstractTransaction.java
@@ -1,23 +1,56 @@
 package transaction;
 
 import java.util.List;
+import java.util.Map;
 
+import com.datastax.driver.core.ConsistencyLevel;
 import com.datastax.driver.core.SimpleStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
+
 public class AbstractTransaction {
     protected Session session;
+    private ConsistencyLevel consistencyLevel = ConsistencyLevel.ALL;
 
     AbstractTransaction(Session s) {
         session = s;
     }
 
     protected List<Row> executeQuery(String query) {
-        // SimpleStatement statement = new SimpleStatement(query).setConsistencyLevel().build();
-        ResultSet res = session.execute(query);
+        Statement statement = new SimpleStatement(query)
+                .setConsistencyLevel(getConsistencyLevel(query));
+        ResultSet res = session.execute(statement);
 
         return res.all();
     }
 
+    protected List<Row> executeQuery(String query, Object... values) {
+        Statement statement = new SimpleStatement(query, values)
+                .setConsistencyLevel(getConsistencyLevel(query));
+        ResultSet res = session.execute(statement);
+
+        return res.all();
+    }
+
+    protected List<Row> executeQuery(String query, Map<String, Object> valueMap) {
+        Statement statement = new SimpleStatement(query, valueMap)
+                .setConsistencyLevel(getConsistencyLevel(query));
+        ResultSet res = session.execute(statement);
+
+        return res.all();
+    }
+
+    private ConsistencyLevel getConsistencyLevel(String query) {
+        if (consistencyLevel.equals(ConsistencyLevel.ALL)) {
+            if (query.startsWith("SELECT")) {
+                return ConsistencyLevel.ONE;
+            } else {
+                return ConsistencyLevel.ALL;
+            }
+        } else {
+            return ConsistencyLevel.QUORUM;
+        }
+    }
 }
