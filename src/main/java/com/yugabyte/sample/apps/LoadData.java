31f1558a370ff5338f0cd1a1505f4541d7289529
Chi Xu <chixucontinue@gmail.com>
2022-10-14 17:53:26 +0800

    init load data

---
+++ src/main/java/com/yugabyte/sample/apps/LoadData.java
@@ -0,0 +1,21 @@
+package com.yugabyte.sample.apps;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+import parser.DataLoader;
+
+public class LoadData {
+    public static void main(String[] args) {
+        Cluster cluster = Cluster.builder()
+                .addContactPoint("localhost")
+                .build();
+        Session session = cluster.connect();
+
+        String schemaPath = args[0];
+        String dataPath = args[1];
+
+        DataLoader dataLoader = new DataLoader(session, schemaPath, dataPath);
+        dataLoader.loadAll();
+    }
+}
