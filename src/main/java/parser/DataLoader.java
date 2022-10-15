2bae576d53d33f94aa14a81e8f5f79523f6784c8
Harry-Gao-H <59434153+Harry-Gao-H@users.noreply.github.com>
2022-10-15 20:06:25 +0800

    Change to Java 11

--- src/main/java/parser/DataLoader.java
+++ src/main/java/parser/DataLoader.java
@@ -72,12 +72,12 @@ public class DataLoader {
     public void loadWarehouse() {
         try {
             String path = dataDir + "/" + WAREHOUSE_FILE;
-            this.runInShell(String.format("""
-                    %s -e USE wholesale;
-                    COPY warehouse
-                    (W_ID, W_NAME, W_STREET_1, W_STREET_2, W_CITY, W_STATE, W_ZIP, W_TAX, W_YTD)
-                    FROM %s WITH DELIMITER = %s;
-                    """,
+            this.runInShell(String.format(
+                    "%s -e USE wholesale;"
+                    + "COPY warehouse"
+                    + "(W_ID, W_NAME, W_STREET_1, W_STREET_2, W_CITY, W_STATE, W_ZIP, W_TAX, W_YTD)"
+                    + "FROM %s WITH DELIMITER = %s;"
+                    ,
                     YCQLSH_PATH, path, DELIM));
         } catch(Exception e) {
             System.err.println(e.toString());
@@ -87,13 +87,13 @@ public class DataLoader {
     public void loadDistrict() {
         try {
             String path = dataDir + "/" + DISTRICT_FILE;
-            this.runInShell(String.format("""
-                    %s -e USE wholesale;
-                    COPY district
-                    (D_W_ID, D_ID, D_NAME, D_STREET_1, D_STREET_2,
-                    D_CITY, D_STATE, D_ZIP, D_TAX, D_YTD, D_NEXT_O_ID)
-                    FROM %s WITH DELIMITER = %s;
-                    """,
+            this.runInShell(String.format(
+                    "%s -e USE wholesale;"
+                    + "COPY district"
+                    + "(D_W_ID, D_ID, D_NAME, D_STREET_1, D_STREET_2,"
+                    + "D_CITY, D_STATE, D_ZIP, D_TAX, D_YTD, D_NEXT_O_ID)"
+                    + "FROM %s WITH DELIMITER = %s;"
+                    ,
                     YCQLSH_PATH, path, DELIM));
         } catch(Exception e) {
             System.err.println(e.toString());
@@ -103,15 +103,15 @@ public class DataLoader {
     public void loadCustomer() {
         try {
             String path = dataDir + "/" + CUSTOMER_FILE;
-            this.runInShell(String.format("""
-                    %s -e USE wholesale;
-                    COPY customer
-                    (C_W_ID, C_D_ID, C_ID, C_FIRST, C_MIDDLE, C_LAST,
-                    C_STREET_1, C_STREET_2, C_CITY, C_STATE, C_ZIP,
-                    C_PHONE, C_SINCE, C_CREDIT, C_CREDIT_LIM, C_DISCOUNT,
-                    C_BALANCE, C_YTD_PAYMENT, C_PAYMENT_CNT, C_DELIVERY_CNT, C_DATA)
-                    FROM %s WITH DELIMITER = %s;
-                    """,
+            this.runInShell(String.format(
+                    "%s -e USE wholesale;"
+                    + "COPY customer"
+                    + "(C_W_ID, C_D_ID, C_ID, C_FIRST, C_MIDDLE, C_LAST,"
+                    + "C_STREET_1, C_STREET_2, C_CITY, C_STATE, C_ZIP,"
+                    + "C_PHONE, C_SINCE, C_CREDIT, C_CREDIT_LIM, C_DISCOUNT,"
+                    + "C_BALANCE, C_YTD_PAYMENT, C_PAYMENT_CNT, C_DELIVERY_CNT, C_DATA)"
+                    + "FROM %s WITH DELIMITER = %s;"
+                    ,
                     YCQLSH_PATH, path, DELIM));
         } catch(Exception e) {
             System.err.println(e.toString());
@@ -121,13 +121,13 @@ public class DataLoader {
     public void loadOrder() {
         try {
             String path = dataDir + "/" + ORDER_FILE;
-            this.runInShell(String.format("""
-                    %s -e USE wholesale;
-                    COPY "order"
-                    (O_W_ID, O_D_ID, O_ID, O_C_ID, O_CARRIER_ID,
-                    O_OL_CNT, O_ALL_LOCAL, O_ENTRY_D)
-                    FROM %s WITH DELIMITER = %s;
-                    """,
+            this.runInShell(String.format(
+                    "%s -e USE wholesale;"
+                    + "COPY \"order\""
+                    + "(O_W_ID, O_D_ID, O_ID, O_C_ID, O_CARRIER_ID,"
+                    + "O_OL_CNT, O_ALL_LOCAL, O_ENTRY_D)"
+                    + "FROM %s WITH DELIMITER = %s;"
+                    ,
                     YCQLSH_PATH, path, DELIM));
         } catch(Exception e) {
             System.err.println(e.toString());
@@ -137,12 +137,12 @@ public class DataLoader {
     public void loadItem() {
         try {
             String path = dataDir + "/" + ITEM_FILE;
-            this.runInShell(String.format("""
-                    %s -e USE wholesale;
-                    COPY item
-                    (I_ID, I_NAME, I_PRICE, I_IM_ID, I_DATA)
-                    FROM %s WITH DELIMITER = %s;
-                    """,
+            this.runInShell(String.format(
+                    "%s -e USE wholesale;"
+                    + "COPY item"
+                    + "(I_ID, I_NAME, I_PRICE, I_IM_ID, I_DATA)"
+                    + "FROM %s WITH DELIMITER = %s;"
+                    ,
                     YCQLSH_PATH, path, DELIM));
         } catch(Exception e) {
             System.err.println(e.toString());
@@ -152,14 +152,14 @@ public class DataLoader {
     public void loadOrderLine() {
         try {
             String path = dataDir + "/" + ORDER_LINE_FILE;
-            this.runInShell(String.format("""
-                    %s -e USE wholesale;
-                    COPY order_line
-                    (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER,
-                    OL_I_ID, OL_DELIVERY_D, OL_AMOUNT, OL_SUPPLY_W_ID,
-                    OL_QUANTITY, OL_DIST_INFO)
-                    FROM %s WITH DELIMITER = %s;
-                    """,
+            this.runInShell(String.format(
+                    "%s -e USE wholesale;"
+                    + "COPY order_line"
+                    + "(OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER,"
+                    + "OL_I_ID, OL_DELIVERY_D, OL_AMOUNT, OL_SUPPLY_W_ID,"
+                    + "OL_QUANTITY, OL_DIST_INFO)"
+                    + "FROM %s WITH DELIMITER = %s;"
+                    ,
                     YCQLSH_PATH, path, DELIM));
         } catch(Exception e) {
             System.err.println(e.toString());
@@ -169,14 +169,14 @@ public class DataLoader {
     public void loadStock() {
         try {
             String path = dataDir + "/" + STOCK_FILE;
-            this.runInShell(String.format("""
-                    %s -e USE wholesale;
-                    COPY stock
-                    (S_W_ID, S_I_ID, S_QUANTITY, S_YTD, S_ORDER_CNT, S_REMOTE_CNT,
-                    S_DIST_01, S_DIST_02, S_DIST_03, S_DIST_04, S_DIST_05, S_DIST_06, S_DIST_07, S_DIST_08, S_DIST_09, S_DIST_10,
-                    S_DATA)
-                    FROM %s WITH DELIMITER = %s;
-                    """,
+            this.runInShell(String.format(
+                    "%s -e USE wholesale;"
+                    + "COPY stock"
+                    + "(S_W_ID, S_I_ID, S_QUANTITY, S_YTD, S_ORDER_CNT, S_REMOTE_CNT,"
+                    + "S_DIST_01, S_DIST_02, S_DIST_03, S_DIST_04, S_DIST_05, S_DIST_06, S_DIST_07, S_DIST_08, S_DIST_09, S_DIST_10,"
+                    + "S_DATA)"
+                    + "FROM %s WITH DELIMITER = %s;"
+                    ,
                     YCQLSH_PATH, path, DELIM));
         } catch(Exception e) {
             System.err.println(e.toString());
