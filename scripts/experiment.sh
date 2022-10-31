88720827664b672d71e5a25dd7e141ac9c3fca98
awp <49505301+wangpalex@users.noreply.github.com>
2022-10-31 15:37:04 +0800

    Update experiment.sh

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -1,19 +1,20 @@
 consistency_level=${1-'all'}
 
-nodes=5
+num_nodes=5
+base_node=20  # base node is xcnd20
 
 submit_job() {
   job_id=$1
   consistency_level=${2-'all'}
   echo "submit job: "$job_id" with consistency level: "$consistency_level
 
-  node_id="xcnd"$((20+$job_id%$nodes))
+  node_id="xcnd"$(($base_node + $job_id % $num_nodes))
   echo "the job will be submitted to node: "$node_id
 
   ssh $node_id "cd Wholesale-YCQL && ./scripts/run.sh ${node_id} ${job_id} ${consistency_level}"
 }
 
-./scripts/dump_data.sh xcnd20
+./scripts/dump_data.sh "xcnd${base_node}"
 if [[ ! -d experiment ]]; then
     mkdir experiment
 fi
@@ -23,5 +24,7 @@ do
   submit_job $c $consistency_level &
 done
 wait
+
+# Generate summary
 /home/stuproj/cs4224i/Wholesale-YCQL/scripts/summary.sh xcnd20
 exit 0
