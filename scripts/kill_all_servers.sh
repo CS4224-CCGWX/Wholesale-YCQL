bf19598ab7d22f67fdf6d43999882b01aae2a661
CS4224i <CS4224i>
2022-10-31 11:39:43 +0800

    add removal of data file

--- scripts/kill_all_servers.sh
+++ scripts/kill_all_servers.sh
@@ -1,10 +1,13 @@
 nodes=5
 
+data_file_dir="/mnt/ramdisk/yugabyte-data/"
 # Kills on each machine.
 for ((c=0; c<$nodes; c++)); do
     nodeID="xcnd"$((20+$c%$nodes))
     ssh $nodeID "cd Wholesale-YCQL && ./scripts/kill_one_server.sh"
     echo "Have killed servers on machine ID=${nodeID}."
+    ssh $nodeID "rm -rf ${data_file_dir}" 
+    echo "Have remove the data file on machine ID=${nodeID}."
 done
 
 exit 0
