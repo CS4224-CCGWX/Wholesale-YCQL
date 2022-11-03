3d5f8b169368a9e02f2ad7447aa33f9a3983cc02
Chi Xu <chixucontinue@gmail.com>
2022-11-03 16:45:57 +0800

    fix

--- src/main/java/transaction/AbstractTransaction.java
+++ src/main/java/transaction/AbstractTransaction.java
@@ -99,38 +99,14 @@ public abstract class AbstractTransaction {
     public void setDefaultConsistencyLevel(String s) {
         ConsistencyLevel level;
         switch(s) {
-        case "any":
-            level = ConsistencyLevel.ANY;
-            break;
         case "one":
             level = ConsistencyLevel.ONE;
-            break;
-        case "two":
-            level = ConsistencyLevel.TWO;
-            break;
-        case "three":
-            level = ConsistencyLevel.THREE;
             break;
         case "quorum":
             level = ConsistencyLevel.QUORUM;
             break;
         case "all":
             level = ConsistencyLevel.ALL;
-            break;
-        case "local_quorum":
-            level = ConsistencyLevel.LOCAL_QUORUM;
-            break;
-        case "each_quorum":
-            level = ConsistencyLevel.EACH_QUORUM;
-            break;
-        case "serial":
-            level = ConsistencyLevel.SERIAL;
-            break;
-        case "local_serial":
-            level = ConsistencyLevel.LOCAL_SERIAL;
-            break;
-        case "local_one":
-            level = ConsistencyLevel.LOCAL_ONE;
             break;
         default:
             level = ConsistencyLevel.ALL;
